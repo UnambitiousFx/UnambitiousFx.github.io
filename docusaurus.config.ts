25cb34edd88381a6c73f2a8fc80c023c5cbc7ff9
Maxime C. <maxime.charles1@proton.me>
2026-06-27 12:46:03 +0200

    Fix bad links (#5)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -122,11 +122,7 @@ const config: Config = {
           items: [
             {
               label: "GitHub",
-              href: "https://github.com/unambitiousfx/unambitiousfx.github.io",
-            },
-            {
-              label: "Main Repository",
-              href: "https://github.com/unambitiousfx/unambitious",
+              href: "https://github.com/unambitiousfx",
             },
           ],
         },
